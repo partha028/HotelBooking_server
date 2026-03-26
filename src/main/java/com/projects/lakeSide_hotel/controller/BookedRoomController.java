db467c29366d818ec0da2ca59af7ff0ccbace96c
partha028 <53009001+partha028@users.noreply.github.com>
2026-03-26 19:26:28 -0400

    US 1943: Add test line

--- src/main/java/com/projects/lakeSide_hotel/controller/BookedRoomController.java
+++ src/main/java/com/projects/lakeSide_hotel/controller/BookedRoomController.java
@@ -1,4 +1,9 @@
 package com.projects.lakeSide_hotel.controller;
 
+import org.springframework.http.ResponseEntity;
+
 public class BookedRoomController {
+    public ResponseEntity<?> getAllBookedRooms() {
+        return ResponseEntity.ok().build();
+    }
 }
